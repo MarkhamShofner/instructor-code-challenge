328db8a88d6a839071885d81bf4573435c66125e
Markham Shofner <markham.shofner@gmail.com>
2015-12-15 13:30:27 -0500

    another debug pass, window can now display text, server not fully operational

--- node-backend-starter-code/server.js
+++ node-backend-starter-code/server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-app.use(bodyParser.json());
+// ^TODO - needed?^
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -31,6 +33,6 @@ app.get('favorites', function(req, res) {
   res.send(data);
 });
 
-app.list(3000, function() {
+app.listen(3000, function() {
   console.log("Listening on port 3000");
 });
